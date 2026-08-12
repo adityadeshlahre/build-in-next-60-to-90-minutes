d3770803a78f16260ec04f67fe40c9679d33c292
Aditya Deshlahre <wymaditya@gmail.com>
2026-08-12 17:27:02 +0530

    fix image download service

--- flickerImageService.js
+++ flickerImageService.js
@@ -20,20 +20,87 @@ class Subscription {
 }
 
 class RateLimiter {
-  // fixed window
+  constructor({ users, subscription, downloadService }) {
+    this.users = users;
+    this.subscription = subscription;
+    this.downloadService = downloadService;
+  }
+
+  proccess(userId, requestId) {
+    const user = this.users.get(userId);
+    const limit = this.subscription.getLimit(user.subscriptionStatus);
+    const state = user.rateLimiterStatus;
+
+    if (Date.now() - state.windowStart >= 60000) {
+      state.requestCount = 0;
+      state.windowStart = Date.now();
+      this.proccessQueue(userId);
+    }
+
+    if (state.requestCount < limit) {
+      this.downloadService.download({ requestId });
+      state.requestCount++;
+    } else {
+      state.queue.enqueue({ userId, requestId });
+    }
+  }
+
+  proccessQueue(userId) {
+    const user = this.users.get(userId);
+    const limit = this.subscription.getLimit(user.subscriptionStatus);
+    const state = user.rateLimiterStatus;
+
+    while (state.queue.length > 0 && state.requestCount < limit) {
+      const request = state.queue.dequeue();
+      this.downloadService.download(request);
+      state.requestCount++;
+    }
+  }
+}
+
+class System {
   constructor() {
-    this.windowStart = 60;
-    this.requestCount = 10;
-    this.queue = new Queue();
+    this.users = new Map();
+    this.subscription = new Subscription();
+    this.downloadService = new DownloadService();
+
+    this.rateLimiter = new RateLimiter({
+      users: this.users,
+      subscription: this.subscription,
+      downloadService: this.downloadService,
+    });
+  }
+
+  createUser({ userId, subscriptionStatus }) {
+    const user = new User({
+      userId,
+      subscriptionStatus,
+      rateLimiterStatus: {
+        windowStart: Date.now(),
+        requestCount: 0,
+        queue: new Queue(),
+      },
+    });
+
+    this.users.set(userId, user);
   }
 }
 
 class Queue {
   constructor() {
-    this.queue = {
-      normal: new Map(),
-      premium: new Map(),
-    }; // FIFO
+    this.queue = [];
+  }
+
+  enqueue(request) {
+    this.queue.push(request);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  get length() {
+    return this.queue.length;
   }
 }
 
